73fbfb5baa0b32ac10d5dfd0d2c8949ca983c37d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-08-10 16:27:07 -0400

    updated dependencies, added styled-components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import './App.scss';
 
-const App = () => <></>;
+const Container = styled.div`
+
+`;
+
+const App = () => (
+	<Container>
+
+	</Container>
+);
 
 export default App;
